871820a6084d716250d9a4a172d18a5e82fdb0d3
F-Papa <francopapa21@gmail.com>
2023-04-11 14:04:17 -0300

    Dependency Inversion UserService

--- src/presentation/user_api.py
+++ src/presentation/user_api.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter
 from src.infrastructure.models.user_dto import UserDTO, UserSignUpDTO
 from src.infrastructure.user_repository_postgresql import UserTable
-import src.usecase.user as use_case
+from src.usecase.user import UserService
+
 
 user_routes = APIRouter(prefix="/user")
 user_repository = UserTable()
+user_service = UserService(UserTable)
 
 #Transaction Model
 @user_routes.get("/{id}")
 async def requests_user_with_id(id: str):
-    return use_case.requests_user_with_id(id)
+    return user_service.requests_user_with_id(id)
 
 @user_routes.put("/", status_code=201)   
 async def wants_to_create_user(user_data: UserSignUpDTO):
-    return use_case.wants_to_create_user(user_data)
+    return user_service.wants_to_create_user(user_data)
--- src/usecase/user.py
+++ src/usecase/user.py
@@ -1,29 +1,30 @@
 from fastapi import APIRouter, status, exceptions
 from src.infrastructure.models.user_dto import UserDTO, UserSignUpDTO
-from src.infrastructure.user_repository_postgresql import UserTable
 from src.infrastructure.firebase import sign_up as firebase_sign_up
-user_routes = APIRouter(prefix="/user")
-user_repository = UserTable()
 
-#Transaction Model
-def requests_user_with_id(id: str):
-    return user_repository.find_by_id(id)
+class UserService():
+    def __init__(self, user_repository):
+        self.user_repository = user_repository    
 
-def wants_to_create_user(user_data: UserSignUpDTO):
-    try:
-        firebase_sign_up(user_data.email, user_data.password)
-    except:
-        raise exceptions.HTTPException(status_code=406, detail="email already exists")
-    try:
-        user_repository.create(user_data)
-    except:
-        raise exceptions.HTTPException(status_code=406, detail="id already exists")
+    #Transaction Model
+    def requests_user_with_id(self, id: str):
+        return self.user_repository.find_by_id(id)
 
-def wants_to_delete_user(user_data: UserDTO):
-    raise NotImplementedError
+    def wants_to_create_user(self, user_data: UserSignUpDTO):
+        try:
+            firebase_sign_up(user_data.email, user_data.password)
+        except:
+            raise exceptions.HTTPException(status_code=406, detail="email already exists")
+        try:
+            self.user_repository.create(user_data)
+        except:
+            raise exceptions.HTTPException(status_code=406, detail="id already exists")
 
-def wants_to_subscribe_to_training(training_id: int):
-    raise NotImplementedError
+    def wants_to_delete_user(self, user_data: UserDTO):
+        raise NotImplementedError
+
+    def wants_to_subscribe_to_training(self, training_id: int):
+        raise NotImplementedError
 
 #@user_routes.get("/")
 # async def users():
