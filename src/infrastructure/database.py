e872009490dd034fd252f3423b672d0d3b4f7d57
F-Papa <francopapa21@gmail.com>
2023-04-12 14:28:33 -0300

    DatabaseURL Production Check

--- src/infrastructure/database.py
+++ src/infrastructure/database.py
@@ -1,16 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-db_user = "users_ihwj_user"
-db_name = "users_ihwj"
-db_pass = "g9F5EtSgbaLDIJJTIZ1aI70ZxruQNt9w"
-db_host_name = "dpg-cgoqtv5269v5rjd5gl20-a"
-db_port = 5432
+try:
+    PRODUCTION_ENV = (os.environ['PRODUCTION'] == 1)
+except:
+    PRODUCTION_ENV = False
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host_name}/{db_name}"
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgres/db" #For Dev Environment
+if PRODUCTION_ENV:
+    db_user = "users_ihwj_user"
+    db_name = "users_ihwj"
+    db_pass = "g9F5EtSgbaLDIJJTIZ1aI70ZxruQNt9w"
+    db_host_name = "dpg-cgoqtv5269v5rjd5gl20-a"
+    db_port = 5432
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host_name}/{db_name}" #For Production
+else:
+    SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgres/db" #For Dev Environment
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
